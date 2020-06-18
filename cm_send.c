a9df273d9629f91cef4f7f3b19d6e1ae84b29988
hs <hs@example.com>
2020-06-18 12:14:11 +0800

    fix

--- cm_send.c
+++ cm_send.c
@@ -20,6 +20,7 @@
 
 #define KERNEL_CM_SEND (unsigned int) 0
 #define KERNEL_CM_RECV (unsigned int) 1
+#define PRINT_MAC	   (unsigned int) 2
 
 int main(int argc, char *argv[])
 {
@@ -28,14 +29,47 @@ int main(int argc, char *argv[])
     int buflen;
     int data;
 
-    buf[0] = inet_addr("10.0.0.5");
+    buf[0] = ntohl(inet_addr("10.0.0.5"));
     fd = open("/dev/cm_rw",O_RDWR);
     if(fd < 0){
         printf("can't open /dev/cm_rw \n");
         return;
     }
 
-    ioctl(fd,KERNEL_CM_SEND,buf);
+    int choose;
+
+while(1){
+    printf("welcome to debug sys in sysfs(later change to debugfs):");
+    printf("1. CM SEND\n");
+    printf("2. CM RECV\n");
+    printf("3. PRINT MAC(use dmesg to check)\n");
+    printf("4. exit\n");
+    printf("please choose:");
+    scanf("%d \n",&choose);
+
+    switch(choose)
+    {
+        case 1:
+        {
+            ioctl(fd,KERNEL_CM_SEND,buf);
+        }
+        case 2:
+        {
+            ioctl(fd,KERNEL_CM_RECV,buf);
+        }
+        case 3:
+        {
+            ioctl(fd,PRINT_MAC,buf);
+        }
+        default:
+        break;
+    }
+    if(choose == 4)
+        break;
+
+    }
+
+    //ioctl(fd,KERNEL_CM_SEND,buf);
     close(fd);
     return 0;
 }
